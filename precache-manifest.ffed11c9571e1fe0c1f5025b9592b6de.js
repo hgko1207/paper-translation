6e00779336460a8806c54930f44b04059ab70186
hgko1207 <khkkhk1207@gmail.com>
2020-03-05 14:43:01 +0900

    Updates

---
+++ precache-manifest.ffed11c9571e1fe0c1f5025b9592b6de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babb786c52d01fc7e1679c291b1766dd",
+    "url": "/paper-translation/index.html"
+  },
+  {
+    "revision": "85799ce672411e76b08f",
+    "url": "/paper-translation/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a24abd0a571db8187541",
+    "url": "/paper-translation/static/css/main.caaf6004.chunk.css"
+  },
+  {
+    "revision": "85799ce672411e76b08f",
+    "url": "/paper-translation/static/js/2.57a9c6e5.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/paper-translation/static/js/2.57a9c6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24abd0a571db8187541",
+    "url": "/paper-translation/static/js/main.0b7e8b71.chunk.js"
+  },
+  {
+    "revision": "92accab61efb53e9e370",
+    "url": "/paper-translation/static/js/runtime-main.7372e008.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paper-translation/precache-manifest.ba542c2c259329a543896b7d27f57e5e.js"
+  "/paper-translation/precache-manifest.ffed11c9571e1fe0c1f5025b9592b6de.js"
 );
 
 self.addEventListener('message', (event) => {
